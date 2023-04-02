f31922a0e36a85aff801e67c0d5b8bedff65d88b
JamerEdiazOlaya <129633173+JamerEdiazOlaya@users.noreply.github.com>
2023-04-02 12:37:55 -0500

    Update flask_login_user_server.py

--- flask_login_user_server.py
+++ flask_login_user_server.py
@@ -15,5 +15,5 @@ def login():
         return jsonify({'status': 'Error', 'message': 'Usuario o contraseña inválidos'})
 
 if __name__ == '__main__':
-    app.run(host='192.168.0.20', port=5000)
+    app.run(host='127.0.0.1', port=5000)
 
